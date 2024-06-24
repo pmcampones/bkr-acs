d8ebfd4e56dfbeb5b0c85c55b8e79d6af621ead4
pcampones <p.campones@campus.fct.unl.pt>
2024-06-24 12:05:02 +0100

    Main for tests...

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fullMembership/broadcast"
+	"fullMembership/network"
+	"os"
+)
+
+type ConcreteObserver struct {
+}
+
+func (co ConcreteObserver) BCBDeliver(msg []byte) {
+	println(string(msg))
+}
+
+func main() {
+	node := network.Join(os.Args[1], os.Args[2], os.Args[3], os.Args[4])
+	observer := ConcreteObserver{}
+	bcbChannel := broadcast.BCBCreateChannel(node, 4, 1)
+	bcbChannel.AttachObserver(observer)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		err := bcbChannel.BCBroadcast([]byte(input.Text()))
+		if err != nil {
+			return
+		}
+	}
+}
