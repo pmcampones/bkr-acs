7897b4c8b09d6ab953985805bf1a9e414e13699b
pcampones <p.campones@campus.fct.unl.pt>
2024-07-18 21:25:16 +0100

    Refactor message tests

--- network/msg_test.go
+++ network/msg_test.go
@@ -9,43 +9,38 @@ import (
 
 func TestShouldReceiveWhatWasSentInSingleMessage(t *testing.T) {
 	msg := []byte("Hello World!!")
-	address := "localhost:6000"
-	listening := make(chan struct{})
-	go func() {
-		listener, err := net.Listen("tcp", address)
-		if err != nil {
-			t.Errorf("unable to listen on address: %v", err)
-			return
-		}
-		listening <- struct{}{}
-		connSend, err := listener.Accept()
-		err = send(connSend, msg)
-		if err != nil {
-			t.Errorf("unable to send message: %v", err)
-			return
-		}
-	}()
-	<-listening
-	connReceive, err := net.Dial("tcp", address)
-	if err != nil {
-		t.Errorf("unable to dial: %v", err)
-		return
-	}
-	received, err := receive(connReceive)
-	if err != nil {
-		t.Errorf("unable to receive message: %v", err)
-		return
-	}
-	assert.Equal(t, received, msg)
+	auxTestShouldReceiveWhatWasSent([][]byte{msg}, t)
 }
 
 func TestShouldReceiveWhatWasSentInMultipleMessages(t *testing.T) {
-	address := "localhost:6000"
 	numMsg := 100000
 	messages := make([][]byte, numMsg)
 	for i := 0; i < numMsg; i++ {
 		messages[i] = []byte(fmt.Sprintf("Hello World!!%d", i))
 	}
+	auxTestShouldReceiveWhatWasSent(messages, t)
+}
+
+func TestShouldReceiveMBLongMessage(t *testing.T) {
+	mb := 1024 * 1024
+	auxTestShouldReceiveLongMessage(mb, t)
+}
+
+func TestShouldReceiveGBLongMessage(t *testing.T) {
+	gb := 1024 * 1024 * 1024
+	auxTestShouldReceiveLongMessage(gb, t)
+}
+
+func auxTestShouldReceiveLongMessage(size int, t *testing.T) {
+	msg := make([]byte, size)
+	for i := 0; i < size; i++ {
+		msg[i] = byte(i % 256)
+	}
+	auxTestShouldReceiveWhatWasSent([][]byte{msg}, t)
+}
+
+func auxTestShouldReceiveWhatWasSent(messages [][]byte, t *testing.T) {
+	address := "localhost:6000"
 	listening := make(chan struct{})
 	go func() {
 		listener, err := net.Listen("tcp", address)
@@ -77,76 +72,4 @@ func TestShouldReceiveWhatWasSentInMultipleMessages(t *testing.T) {
 		}
 		assert.Equal(t, string(received), string(msg))
 	}
-}
-
-func TestShouldReceiveMBLongMessage(t *testing.T) {
-	mb := 1024 * 1024
-	msg := make([]byte, mb)
-	for i := 0; i < mb; i++ {
-		msg[i] = byte(i % 256)
-	}
-	address := "localhost:6000"
-	listening := make(chan struct{})
-	go func() {
-		listener, err := net.Listen("tcp", address)
-		if err != nil {
-			t.Errorf("unable to listen on address: %v", err)
-			return
-		}
-		listening <- struct{}{}
-		connSend, err := listener.Accept()
-		err = send(connSend, msg)
-		if err != nil {
-			t.Errorf("unable to send message: %v", err)
-			return
-		}
-	}()
-	<-listening
-	connReceive, err := net.Dial("tcp", address)
-	if err != nil {
-		t.Errorf("unable to dial: %v", err)
-		return
-	}
-	received, err := receive(connReceive)
-	if err != nil {
-		t.Errorf("unable to receive message: %v", err)
-		return
-	}
-	assert.Equal(t, received, msg)
-}
-
-func TestShouldReceiveGBLongMessage(t *testing.T) {
-	mb := 1024 * 1024 * 1024
-	msg := make([]byte, mb)
-	for i := 0; i < mb; i++ {
-		msg[i] = byte(i % 256)
-	}
-	address := "localhost:6000"
-	listening := make(chan struct{})
-	go func() {
-		listener, err := net.Listen("tcp", address)
-		if err != nil {
-			t.Errorf("unable to listen on address: %v", err)
-			return
-		}
-		listening <- struct{}{}
-		connSend, err := listener.Accept()
-		err = send(connSend, msg)
-		if err != nil {
-			t.Errorf("unable to send message: %v", err)
-			return
-		}
-	}()
-	<-listening
-	connReceive, err := net.Dial("tcp", address)
-	if err != nil {
-		t.Errorf("unable to dial: %v", err)
-		return
-	}
-	received, err := receive(connReceive)
-	if err != nil {
-		t.Errorf("unable to receive message: %v", err)
-		return
-	}
-	assert.Equal(t, received, msg)
 }
