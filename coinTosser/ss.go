265fa4989684a77942adb29eabaec76803e4a8b2
pmcampones <p.campones@campus.fct.unl.pt>
2024-10-22 14:42:39 +0100

    Refactor SS utilities and removal of old deal file

--- coinTosser/ss.go
+++ coinTosser/ss.go
@@ -10,16 +10,18 @@ import (
 	"github.com/samber/lo"
 )
 
-const scalarSize = 32
-const pointSize = 32
-const shareSize = scalarSize * 2
-const pointShareSize = scalarSize + pointSize
-
 // pointShare is a secret share hidden in a group operation.
 // This is used in the coin tossing scheme to hide the secret while making it usable as a randomness source.
 type pointShare struct {
 	id    group.Scalar
 	point group.Element
+}
+
+func shareToPoint(share secretsharing.Share, base group.Element) pointShare {
+	return pointShare{
+		id:    share.ID,
+		point: mulPoint(base, share.Value),
+	}
 }
 
 func emptyPointShare() pointShare {
@@ -59,6 +61,96 @@ func (ps *pointShare) unmarshalBinary(data []byte) error {
 	return nil
 }
 
+func shareSecret(threshold uint, nodes uint, secret group.Scalar) []secretsharing.Share {
+	secretSharing := secretsharing.New(rand.Reader, threshold, secret)
+	return secretSharing.Share(nodes)
+}
+
+func recoverSecretFromPoints(shares []pointShare) group.Element {
+	indices := lo.Map(shares, func(share pointShare, _ int) group.Scalar { return share.id })
+	coefficients := lo.Map(indices, func(i group.Scalar, _ int) group.Scalar { return lagrangeCoefficient(i, indices) })
+	terms := lo.ZipBy2(shares, coefficients, func(share pointShare, coeff group.Scalar) group.Element {
+		return mulPoint(share.point, coeff)
+	})
+	return lo.Reduce(terms[1:], func(acc group.Element, term group.Element, _ int) group.Element {
+		return addPoint(acc, term)
+	}, terms[0])
+}
+
+func lagrangeCoefficient(i group.Scalar, indices []group.Scalar) group.Scalar {
+	filteredIndices := lo.Filter(indices, func(j group.Scalar, _ int) bool { return !i.IsEqual(j) })
+	numerators := lo.Reduce(filteredIndices, func(acc group.Scalar, j group.Scalar, _ int) group.Scalar {
+		return mulScalar(neg(j), acc)
+	}, newScalar(uint64(1)))
+	denominators := lo.Reduce(filteredIndices, func(acc group.Scalar, j group.Scalar, _ int) group.Scalar {
+		return mulScalar(acc, sub(i, j))
+	}, newScalar(uint64(1)))
+	return mulScalar(numerators, inv(denominators))
+}
+
+// hashPointToBool hashes a point to a boolean value
+// Not sure if this is secure and unbiased.
+func hashPointToBool(point group.Element) (bool, error) {
+	pointMarshal, err := point.MarshalBinary()
+	if err != nil {
+		return false, fmt.Errorf("unable to generate bytes from point: %v", err)
+	}
+	hashed := sha256.Sum256(pointMarshal)
+	sum := lo.Reduce(hashed[:], func(acc int, b byte, _ int) int { return acc + int(b) }, 0)
+	return sum%2 == 0, nil
+}
+
+func areScalarEquals(a, b group.Scalar) (bool, error) {
+	aBytes, err := a.MarshalBinary()
+	if err != nil {
+		return false, fmt.Errorf("unable to marshal scalar: %v", err)
+	}
+	bBytes, err := b.MarshalBinary()
+	if err != nil {
+		return false, fmt.Errorf("unable to marshal scalar: %v", err)
+	}
+	return bytes.Equal(aBytes, bBytes), nil
+}
+
+func areElementsEquals(a, b group.Element) (bool, error) {
+	aBytes, err := a.MarshalBinary()
+	if err != nil {
+		return false, fmt.Errorf("unable to marshal scalar: %v", err)
+	}
+	bBytes, err := b.MarshalBinary()
+	if err != nil {
+		return false, fmt.Errorf("unable to marshal scalar: %v", err)
+	}
+	return bytes.Equal(aBytes, bBytes), nil
+}
+
+func marshalShare(share secretsharing.Share) ([]byte, error) {
+	idBytes, err := share.ID.MarshalBinary()
+	if err != nil {
+		return nil, fmt.Errorf("unable to marshal share ID: %v", err)
+	}
+	valueBytes, err := share.Value.MarshalBinary()
+	if err != nil {
+		return nil, fmt.Errorf("unable to marshal share value: %v", err)
+	}
+	return append(idBytes, valueBytes...), nil
+}
+
+func unmarshalShare(data []byte) (secretsharing.Share, error) {
+	id := group.Ristretto255.NewScalar()
+	value := group.Ristretto255.NewScalar()
+	if scalarSize, err := getScalarSize(); err != nil {
+		return secretsharing.Share{}, fmt.Errorf("unable to get scalar size: %v", err)
+	} else if len(data) != scalarSize*2 {
+		return secretsharing.Share{}, fmt.Errorf("argument has incorrect size: got %d bytes, expected %d", len(data), scalarSize*2)
+	} else if err := id.UnmarshalBinary(data[:scalarSize]); err != nil {
+		return secretsharing.Share{}, fmt.Errorf("unable to unmarshal share ID: %v", err)
+	} else if err := value.UnmarshalBinary(data[scalarSize:]); err != nil {
+		return secretsharing.Share{}, fmt.Errorf("unable to unmarshal share value: %v", err)
+	}
+	return secretsharing.Share{ID: id, Value: value}, nil
+}
+
 func getScalarSize() (int, error) {
 	scalar := group.Ristretto255.NewScalar()
 	scalarBytes, err := scalar.MarshalBinary()
@@ -87,137 +179,4 @@ func getPointShareSize() (int, error) {
 		return 0, fmt.Errorf("unable to get element size: %v", err)
 	}
 	return scalarSize + elementSize, nil
-}
-
-func ShareSecret(threshold uint, nodes uint, secret group.Scalar) []secretsharing.Share {
-	secretSharing := secretsharing.New(rand.Reader, threshold, secret)
-	return secretSharing.Share(nodes)
-}
-
-func RecoverSecret(threshold uint, shares []secretsharing.Share) (group.Scalar, error) {
-	return secretsharing.Recover(threshold, shares)
-}
-
-func ShareToPoint(share secretsharing.Share, base group.Element) pointShare {
-	return pointShare{
-		id:    share.ID,
-		point: mulPoint(base, share.Value),
-	}
-}
-
-func RecoverSecretFromPoints(shares []pointShare) group.Element {
-	indices := lo.Map(shares, func(share pointShare, _ int) group.Scalar { return share.id })
-	coefficients := lo.Map(indices, func(i group.Scalar, _ int) group.Scalar { return lagrangeCoefficient(i, indices) })
-	terms := lo.ZipBy2(shares, coefficients, func(share pointShare, coeff group.Scalar) group.Element {
-		return mulPoint(share.point, coeff)
-	})
-	return lo.Reduce(terms[1:], func(acc group.Element, term group.Element, _ int) group.Element {
-		return addPoint(acc, term)
-	}, terms[0])
-}
-
-func lagrangeCoefficient(i group.Scalar, indices []group.Scalar) group.Scalar {
-	filteredIndices := lo.Filter(indices, func(j group.Scalar, _ int) bool { return !i.IsEqual(j) })
-	numerators := lo.Reduce(filteredIndices, func(acc group.Scalar, j group.Scalar, _ int) group.Scalar {
-		return mulScalar(neg(j), acc)
-	}, newScalar(uint64(1)))
-	denominators := lo.Reduce(filteredIndices, func(acc group.Scalar, j group.Scalar, _ int) group.Scalar {
-		return mulScalar(acc, sub(i, j))
-	}, newScalar(uint64(1)))
-	return mulScalar(numerators, inv(denominators))
-}
-
-// HashPointToBool hashes a point to a boolean value
-// Not sure if this is secure and unbiased.
-func HashPointToBool(point group.Element) (bool, error) {
-	pointMarshal, err := point.MarshalBinary()
-	if err != nil {
-		return false, fmt.Errorf("unable to generate bytes from point: %v", err)
-	}
-	hashed := sha256.Sum256(pointMarshal)
-	sum := lo.Reduce(hashed[:], func(acc int, b byte, _ int) int { return acc + int(b) }, 0)
-	return sum%2 == 0, nil
-}
-
-func marshalShare(share secretsharing.Share) ([]byte, error) {
-	idBytes, err := share.ID.MarshalBinary()
-	if err != nil {
-		return nil, fmt.Errorf("unable to marshal share ID: %v", err)
-	}
-	valueBytes, err := share.Value.MarshalBinary()
-	if err != nil {
-		return nil, fmt.Errorf("unable to marshal share value: %v", err)
-	}
-	return append(idBytes, valueBytes...), nil
-}
-
-func unmarshalShare(shareBytes []byte) (secretsharing.Share, error) {
-	if len(shareBytes) != shareSize {
-		return secretsharing.Share{}, fmt.Errorf("argument has incorrect size: got %d bytes, expected %d", len(shareBytes), shareSize)
-	}
-	buffer := make([]byte, scalarSize)
-	reader := bytes.NewReader(shareBytes)
-	num, err := reader.Read(buffer)
-	if err != nil {
-		return secretsharing.Share{}, fmt.Errorf("unable to read share ID bytes: %v", err)
-	} else if num != scalarSize {
-		return secretsharing.Share{}, fmt.Errorf("unable to read share ID bytes: read %d bytes, expected %d", num, scalarSize)
-	}
-	id := group.Ristretto255.NewScalar()
-	if err := id.UnmarshalBinary(buffer); err != nil {
-		return secretsharing.Share{}, fmt.Errorf("unable to unmarshal share ID: %v", err)
-	}
-	num, err = reader.Read(buffer)
-	if err != nil {
-		return secretsharing.Share{}, fmt.Errorf("unable to read share Value bytes: %v", err)
-	} else if num != scalarSize {
-		return secretsharing.Share{}, fmt.Errorf("unable to read share Value bytes: read %d bytes, expected %d", num, scalarSize)
-	}
-	value := group.Ristretto255.NewScalar()
-	if err := value.UnmarshalBinary(buffer); err != nil {
-		return secretsharing.Share{}, fmt.Errorf("unable to unmarshal share value: %v", err)
-	}
-	return secretsharing.Share{ID: id, Value: value}, nil
-}
-
-func marshalPointShare(share pointShare) ([]byte, error) {
-	idBytes, err := share.id.MarshalBinary()
-	if err != nil {
-		return nil, fmt.Errorf("unable to marshal share ID: %v", err)
-	}
-	pointBytes, err := share.point.MarshalBinary()
-	if err != nil {
-		return nil, fmt.Errorf("unable to marshal share value: %v", err)
-	}
-	return append(idBytes, pointBytes...), nil
-}
-
-func unmarshalPointShare(shareBytes []byte) (pointShare, error) {
-	if len(shareBytes) != pointShareSize {
-		return pointShare{}, fmt.Errorf("argument has incorrect size: got %d bytes, expected %d", len(shareBytes), pointShareSize)
-	}
-	idBuffer := make([]byte, scalarSize)
-	reader := bytes.NewReader(shareBytes)
-	num, err := reader.Read(idBuffer)
-	if err != nil {
-		return pointShare{}, fmt.Errorf("unable to read share ID bytes: %v", err)
-	} else if num != scalarSize {
-		return pointShare{}, fmt.Errorf("unable to read share ID bytes: read %d bytes, expected %d", num, scalarSize)
-	}
-	id := group.Ristretto255.NewScalar()
-	if err := id.UnmarshalBinary(idBuffer); err != nil {
-		return pointShare{}, fmt.Errorf("unable to unmarshal share ID: %v", err)
-	}
-	pointBuffer := make([]byte, pointSize)
-	num, err = reader.Read(pointBuffer)
-	if err != nil {
-		return pointShare{}, fmt.Errorf("unable to read share Value bytes: %v", err)
-	} else if num != pointSize {
-		return pointShare{}, fmt.Errorf("unable to read share Value bytes: read %d bytes, expected %d", num, pointSize)
-	}
-	point := group.Ristretto255.NewElement()
-	if err := point.UnmarshalBinary(pointBuffer); err != nil {
-		return pointShare{}, fmt.Errorf("unable to unmarshal share value: %v", err)
-	}
-	return pointShare{id: id, point: point}, nil
 }
--- coinTosser/ss_test.go
+++ coinTosser/ss_test.go
@@ -4,13 +4,11 @@ import (
 	"crypto"
 	"crypto/rand"
 	"encoding/hex"
-	"fmt"
 	"github.com/cloudflare/circl/group"
 	"github.com/cloudflare/circl/secretsharing"
 	"github.com/cloudflare/circl/zk/dleq"
-	"github.com/magiconair/properties/assert"
 	"github.com/samber/lo"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -19,13 +17,13 @@ func TestRecoverSecret(t *testing.T) {
 	nodes := uint(50)
 	threshold := uint(20)
 	secret := g.NewScalar().SetUint64(1234567890)
-	shares := ShareSecret(threshold, nodes, secret)
-	require.Equal(t, int(nodes), len(shares))
-	recov1, err := RecoverSecret(threshold, shares[:threshold+1])
-	require.NoError(t, err)
+	shares := shareSecret(threshold, nodes, secret)
+	assert.Equal(t, int(nodes), len(shares))
+	recov1, err := secretsharing.Recover(threshold, shares[:threshold+1])
+	assert.NoError(t, err)
 	assert.Equal(t, secret, recov1)
-	recov2, err := RecoverSecret(threshold, shares[1:threshold+2])
-	require.NoError(t, err)
+	recov2, err := secretsharing.Recover(threshold, shares[1:threshold+2])
+	assert.NoError(t, err)
 	assert.Equal(t, secret, recov2)
 }
 
@@ -35,14 +33,14 @@ func TestSSWithPoints(t *testing.T) {
 	threshold := uint(20)
 	secret := g.NewScalar().SetUint64(1234567890)
 	base := g.HashToElement([]byte("base"), []byte("ss_tests"))
-	shares := ShareSecret(threshold, nodes, secret)
+	shares := shareSecret(threshold, nodes, secret)
 	pointSecret := g.Identity().Mul(base, secret)
-	hiddenShares := lo.Map(shares, func(share secretsharing.Share, _ int) pointShare { return ShareToPoint(share, base) })
-	recov1 := RecoverSecretFromPoints(hiddenShares[:])
+	hiddenShares := lo.Map(shares, func(share secretsharing.Share, _ int) pointShare { return shareToPoint(share, base) })
+	recov1 := recoverSecretFromPoints(hiddenShares[:])
 	ptSecBytes, err := pointSecret.MarshalBinary()
-	require.NoError(t, err)
+	assert.NoError(t, err)
 	recSecBytes, err := recov1.MarshalBinary()
-	require.NoError(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, recSecBytes, ptSecBytes)
 }
 
@@ -63,18 +61,18 @@ func TestDLEquivalence(t *testing.T) {
 	H := g.RandomElement(rand.Reader)
 	B := g.NewElement().Mul(H, x)
 
-	fmt.Printf("Value to prove: %v\n", x)
-	fmt.Printf("Public value (A):\n%v\n", A)
-	fmt.Printf("Public value (B):\n%v\n", B)
-	fmt.Printf("Domain seperation: %s\n\n", dst)
+	t.Logf("Value to prove: %v\n", x)
+	t.Logf("Public value (A):\n%v\n", A)
+	t.Logf("Public value (B):\n%v\n", B)
+	t.Logf("Domain seperation: %s\n\n", dst)
 
-	rr := g.HashToScalar([]byte("randomness"), []byte("randomness"))
+	rr := g.HashToScalar([]byte("randomness"), []byte("ss_test"))
 	proof, _ := prover.ProveWithRandomness(x, G, A, H, B, rr)
 	p, _ := proof.MarshalBinary()
-	fmt.Printf("\nProof with randomness: %+v\n", hex.EncodeToString(p))
+	t.Logf("\nProof with randomness: %+v\n", hex.EncodeToString(p))
 	rtn := verifier.Verify(G, A, H, B, proof)
 
-	fmt.Printf("Verification: %+v\n", rtn)
+	t.Logf("Verification: %+v\n", rtn)
 }
 
 func TestDLEquivalenceManyShares(t *testing.T) {
@@ -88,14 +86,13 @@ func TestDLEquivalenceManyShares(t *testing.T) {
 	secret := g.NewScalar().SetUint64(1234567890)
 	commitBase := g.HashToElement([]byte("commit"), []byte("point"))
 	randomBase := g.HashToElement([]byte("randomBase"), []byte("1"))
-	shares := ShareSecret(threshold, nodes, secret)
+	shares := shareSecret(threshold, nodes, secret)
 	rnd := g.HashToScalar([]byte("randomVal"), []byte("1"))
 	for _, share := range shares {
-		fmt.Println("Share idx:", share.ID)
 		hiddenShare := g.NewElement().Mul(randomBase, share.Value)
 		commitment := g.NewElement().Mul(commitBase, share.Value)
 		proof, err := prover.ProveWithRandomness(share.Value, randomBase, hiddenShare, commitBase, commitment, rnd)
-		require.NoError(t, err)
+		assert.NoError(t, err)
 		res := verifier.Verify(randomBase, hiddenShare, commitBase, commitment, proof)
 		assert.Equal(t, res, true)
 	}
@@ -104,33 +101,34 @@ func TestDLEquivalenceManyShares(t *testing.T) {
 func TestHashPointToBool(t *testing.T) {
 	g := group.Ristretto255
 	point := g.HashToElement([]byte("base"), []byte("point"))
-	b, err := HashPointToBool(point)
-	require.NoError(t, err)
-	fmt.Println(b)
+	b, err := hashPointToBool(point)
+	assert.NoError(t, err)
+	t.Log(b)
 }
 
 func TestMarshalAndUnmarshal(t *testing.T) {
 	g := group.Ristretto255
 	ogShare := secretsharing.Share{ID: g.NewScalar().SetUint64(1), Value: g.NewScalar().SetUint64(2)}
 	shareBytes, err := marshalShare(ogShare)
-	require.NoError(t, err)
+	assert.NoError(t, err)
 	recovShare, err := unmarshalShare(shareBytes)
-	require.NoError(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, ogShare, recovShare)
 }
 
 func TestMarshalPointShare(t *testing.T) {
 	g := group.Ristretto255
-	base := g.HashToElement([]byte("base"), []byte("point"))
+	base := g.HashToElement([]byte("base"), []byte("ss_test"))
 	share := pointShare{id: g.NewScalar().SetUint64(1), point: base}
-	shareBytes, err := marshalPointShare(share)
-	require.NoError(t, err)
-	recovShare, err := unmarshalPointShare(shareBytes)
-	require.NoError(t, err)
+	shareBytes, err := share.marshalBinary()
+	assert.NoError(t, err)
+	recovShare := emptyPointShare()
+	err = recovShare.unmarshalBinary(shareBytes)
+	assert.NoError(t, err)
 	assert.Equal(t, share.id, recovShare.id)
 	sharePtBytes, err := share.point.MarshalBinary()
-	require.NoError(t, err)
+	assert.NoError(t, err)
 	recovPtBytes, err := recovShare.point.MarshalBinary()
-	require.NoError(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, sharePtBytes, recovPtBytes)
 }
