78e1ffb0d0a001a2490c31cc0c5dacc973c00efb
pmcampones <p.campones@campus.fct.unl.pt>
2024-10-30 14:51:37 +0000

    Tests for decision when all correct nodes propose the same and the remaining nodes either crash or behave erratically

--- aba/mmr_test.go
+++ aba/mmr_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
+	"math/rand/v2"
 	"testing"
 )
 
@@ -26,24 +27,74 @@ func testShouldDecideSelfProposedValue(t *testing.T, proposal byte) {
 }
 
 func TestShouldDecideMultipleNoFailuresAllPropose0(t *testing.T) {
-	testShouldDecideMultipleNoFailuresAllProposeSame(t, 0)
+	n := uint(10)
+	testShouldDecideMultipleAllProposeSame(t, n, n, 0, 0, 0)
 }
 
 func TestShouldDecideMultipleNoFailuresAllPropose1(t *testing.T) {
-	testShouldDecideMultipleNoFailuresAllProposeSame(t, 1)
+	n := uint(10)
+	testShouldDecideMultipleAllProposeSame(t, n, n, 0, 0, 1)
 }
 
-func testShouldDecideMultipleNoFailuresAllProposeSame(t *testing.T, proposal byte) {
-	n := 10
+func TestShouldDecideMultipleMaxFailuresAllPropose0(t *testing.T) {
+	f := uint(3)
+	n := 3*f + 1
+	testShouldDecideMultipleAllProposeSame(t, n, n, f, 0, 0)
+}
+
+func TestShouldDecideMultipleMaxFailuresAllPropose1(t *testing.T) {
+	f := uint(3)
+	n := 3*f + 1
+	testShouldDecideMultipleAllProposeSame(t, n, n, f, 0, 1)
+}
+
+func TestShouldDecideMultipleMaxCrashAllPropose0(t *testing.T) {
+	f := uint(3)
+	n := 3*f + 1
+	testShouldDecideMultipleAllProposeSame(t, n, n-f, f, 0, 0)
+}
+
+func TestShouldDecideMultipleMaxCrashAllPropose1(t *testing.T) {
+	f := uint(3)
+	n := 3*f + 1
+	testShouldDecideMultipleAllProposeSame(t, n, n-f, f, 0, 1)
+}
+
+func TestShouldDecideMultipleMaxByzantinePropose0(t *testing.T) {
+	f := uint(3)
+	n := 3*f + 1
+	testShouldDecideMultipleAllProposeSame(t, n, n-f, f, f, 0)
+}
+
+func TestShouldDecideMultipleMaxByzantinePropose1(t *testing.T) {
+	f := uint(3)
+	n := 3*f + 1
+	testShouldDecideMultipleAllProposeSame(t, n, n-f, f, f, 1)
+}
+
+func testShouldDecideMultipleAllProposeSame(t *testing.T, n, correct, f, byzantine uint, proposal byte) {
 	s := newOrderedMMRScheduler()
-	nodes := lo.Map(lo.Range(n), func(_ int, _ int) uuid.UUID { return uuid.New() })
-	wmmrs := lo.Map(nodes, func(node uuid.UUID, _ int) *wrappedMMR { return s.getChannels(t, uint(n), 0, node) })
+	nodes := lo.Map(lo.Range(int(correct)), func(_ int, _ int) uuid.UUID { return uuid.New() })
+	wmmrs := lo.Map(nodes, func(node uuid.UUID, _ int) *wrappedMMR { return s.getChannels(t, n, f, node) })
+	byzIds := lo.Map(lo.Range(int(byzantine)), func(_ int, _ int) uuid.UUID { return uuid.New() })
+	for _, byzId := range byzIds {
+		for _, wmmr := range wmmrs {
+			dec, err := wmmr.m.submitDecision(byte(1-proposal), byzId)
+			assert.NoError(t, err)
+			assert.Equal(t, bot, dec)
+			for r := 0; r < 20; r++ {
+				val := rand.IntN(2)
+				assert.NoError(t, wmmr.m.submitBVal(byte(val), byzId, uint16(r)))
+				assert.NoError(t, wmmr.m.submitAux(byte(val), byzId, uint16(r)))
+			}
+		}
+	}
 	for _, wmmr := range wmmrs {
 		assert.NoError(t, wmmr.m.propose(proposal))
 	}
 	finalDecisions := lo.Map(wmmrs, func(wmmr *wrappedMMR, _ int) byte { return <-wmmr.decision })
 	assert.True(t, lo.EveryBy(finalDecisions, func(decision byte) bool { return decision == proposal }))
-	for _, wmmr := range wmmrs {
-		wmmr.m.close()
-	}
+	//for _, wmmr := range wmmrs {
+	//	wmmr.m.close()
+	//}
 }
