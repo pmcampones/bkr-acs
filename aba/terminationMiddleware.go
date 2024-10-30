21bd88bbd97a01f1e72b97cb5aaf7e80f137160c
pmcampones <p.campones@campus.fct.unl.pt>
2024-10-30 18:37:25 +0000

    terminationMiddleware is unique for all ABA instances

--- aba/terminationMiddleware.go
+++ aba/terminationMiddleware.go
@@ -1,7 +1,9 @@
 package aba
 
 import (
+	"bufio"
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"github.com/google/uuid"
 	"log/slog"
@@ -11,64 +13,84 @@ import (
 
 var termLogger = utils.GetLogger(slog.LevelDebug)
 
-type terminationGadget struct {
-	roundPrefix []byte
-	brb         *brb.BRBChannel
-	received    map[uuid.UUID]bool
-	results     []uint
-	f           uint
-	output      chan byte
+type terminationMsg struct {
+	sender   uuid.UUID
+	instance uuid.UUID
+	round    uint16
+	decision byte
 }
 
-func newTerminationGadget(roundPrefix []byte, brb *brb.BRBChannel, f uint) *terminationGadget {
-	tg := &terminationGadget{
-		roundPrefix: roundPrefix,
-		brb:         brb,
-		received:    make(map[uuid.UUID]bool),
-		results:     []uint{0, 0},
-		f:           f,
-		output:      make(chan byte),
+type terminationMiddleware struct {
+	brb       *brb.BRBChannel
+	output    chan *terminationMsg
+	closeChan chan struct{}
+}
+
+func newTerminationMiddleware(brb *brb.BRBChannel) *terminationMiddleware {
+	tg := &terminationMiddleware{
+		brb:       brb,
+		output:    make(chan *terminationMsg),
+		closeChan: make(chan struct{}),
 	}
 	go tg.listenDecisions()
 	return tg
 }
 
-func (tg *terminationGadget) listenDecisions() {
-	for brbMsg := range tg.brb.BrbDeliver {
-		msg := brbMsg.Content
-		if tg.pertainsThisRound(msg) {
-			err := tg.processMsg(msg[len(tg.roundPrefix):], brbMsg.Sender)
+func (m *terminationMiddleware) listenDecisions() {
+	for {
+		select {
+		case brbMsg := <-m.brb.BrbDeliver:
+			tm, err := m.parseMsg(brbMsg.Content, brbMsg.Sender)
 			if err != nil {
-				termLogger.Warn("unable to process message", "error", err)
+				termLogger.Warn("unable to parse termination message", "error", err)
+			} else {
+				termLogger.Debug("received termination message", "sender", tm.sender, "instance", tm.instance, "round", tm.round, "decision", tm.decision)
+				go func() { m.output <- tm }()
 			}
+		case <-m.closeChan:
+			termLogger.Info("closing termination gadget")
+			return
 		}
 	}
 }
 
-func (tg *terminationGadget) processMsg(msg []byte, sender uuid.UUID) error {
-	if tg.received[sender] {
-		return fmt.Errorf("duplicate message from %s", sender)
-	} else if len(msg) != 1 {
-		return fmt.Errorf("invalid message length %d, expected single byte", len(msg))
-	} else if decision := msg[0]; decision > 1 {
-		return fmt.Errorf("invalid decision %d", decision)
+func (m *terminationMiddleware) parseMsg(msg []byte, sender uuid.UUID) (*terminationMsg, error) {
+	tm := &terminationMsg{}
+	tm.sender = sender
+	reader := bytes.NewReader(msg)
+	if id, err := utils.ExtractIdFromMessage(reader); err != nil {
+		return nil, fmt.Errorf("unable to extract instance id from termination message: %v", err)
 	} else {
-		tg.received[sender] = true
-		tg.results[decision]++
-		if tg.results[decision] > tg.f {
-			tg.output <- decision
-		}
+		tm.instance = id
+	}
+	if err := binary.Read(reader, binary.LittleEndian, &tm.round); err != nil {
+		return nil, fmt.Errorf("unable to read round from termination message: %v", err)
+	} else if err := binary.Read(reader, binary.LittleEndian, &tm.decision); err != nil {
+		return nil, fmt.Errorf("unable to read decision from termination message: %v", err)
+	}
+	return tm, nil
+}
+
+func (m *terminationMiddleware) broadcastDecision(instance uuid.UUID, round uint16, decision byte) error {
+	buf := bytes.NewBuffer([]byte{})
+	writer := bufio.NewWriter(buf)
+	if idBytes, err := instance.MarshalBinary(); err != nil {
+		return fmt.Errorf("unable to marshal instance id: %v", err)
+	} else if _, err := writer.Write(idBytes); err != nil {
+		return fmt.Errorf("unable to write instance id to termination message: %v", err)
+	} else if err := binary.Write(writer, binary.LittleEndian, round); err != nil {
+		return fmt.Errorf("unable to write round to termination message: %v", err)
+	} else if err := binary.Write(writer, binary.LittleEndian, decision); err != nil {
+		return fmt.Errorf("unable to write decision to termination message: %v", err)
+	} else if err := writer.Flush(); err != nil {
+		return fmt.Errorf("unable to flush termination message buffer: %v", err)
+	} else if err := m.brb.BRBroadcast(buf.Bytes()); err != nil {
+		return fmt.Errorf("unable to broadcast termination message: %v", err)
 	}
 	return nil
 }
 
-func (tg *terminationGadget) pertainsThisRound(msg []byte) bool {
-	return bytes.HasPrefix(msg, tg.roundPrefix)
-}
-
-func (tg *terminationGadget) decide(decision byte) error {
-	if err := tg.brb.BRBroadcast(append(tg.roundPrefix, decision)); err != nil {
-		return fmt.Errorf("unable to broadcast decision: %w", err)
-	}
-	return nil
+func (m *terminationMiddleware) close() {
+	termLogger.Info("signaling close termination middleware")
+	m.closeChan <- struct{}{}
 }
--- aba/terminationMiddleware_test.go
+++ aba/terminationMiddleware_test.go
@@ -1,78 +1,27 @@
 package aba
 
 import (
-	"fmt"
-	"github.com/samber/lo"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"pace/brb"
 	on "pace/overlayNetwork"
 	"testing"
-	"time"
 )
 
-func TestShouldIgnoreMessagesFromOtherABA(t *testing.T) {
+func TestMarshalAndUnmarshalTerminationMessage(t *testing.T) {
+	abaInstance := uuid.New()
+	round := uint16(42)
+	decision := byte(1)
 	node := on.GetNode(t, "localhost:6000", "localhost:6000")
-	beb := on.CreateBEBChannel(node, 'A')
-	brbDeliver := make(chan brb.BRBMsg)
-	brbChan := brb.CreateBRBChannel(1, 0, beb, brbDeliver)
+	bebChannel := on.CreateBEBChannel(node, 't')
+	brbChannel := brb.CreateBRBChannel(1, 0, bebChannel, make(chan brb.BRBMsg))
 	on.InitializeNodes(t, []*on.Node{node})
-	tg := newTerminationGadget([]byte("prefix"), brbChan, 0)
-	assert.NoError(t, brbChan.BRBroadcast([]byte("This message should be ignored")))
-	tick := time.Tick(100 * time.Millisecond)
-	select {
-	case <-tg.output:
-		t.Error("Should not have received a termination signal")
-	case <-tick:
-	}
-	brbChan.Close()
-	assert.NoError(t, node.Disconnect())
-}
-
-func TestShouldTerminateWithOwnDecision(t *testing.T) {
-	prefix := []byte("prefix")
-	node := on.GetNode(t, "localhost:6000", "localhost:6000")
-	beb := on.CreateBEBChannel(node, 'A')
-	brbDeliver := make(chan brb.BRBMsg)
-	brbChan := brb.CreateBRBChannel(1, 0, beb, brbDeliver)
-	on.InitializeNodes(t, []*on.Node{node})
-	tg := newTerminationGadget(prefix, brbChan, 0)
-	assert.NoError(t, brbChan.BRBroadcast(append(prefix, 0)))
-	decision := <-tg.output
-	assert.Equal(t, byte(0), decision)
-	brbChan.Close()
-	assert.NoError(t, node.Disconnect())
-}
-
-func TestShouldWaitForThresholdDecisions(t *testing.T) {
-	f := uint(3)
-	n := 3*f + 1
-	prefix := []byte("prefix")
-	nodes := lo.Map(lo.Range(int(n)), func(i int, _ int) *on.Node {
-		address := fmt.Sprintf("localhost:%d", 6000+i)
-		return on.GetNode(t, address, "localhost:6000")
-	})
-	bebs := lo.Map(nodes, func(node *on.Node, _ int) *on.BEBChannel { return on.CreateBEBChannel(node, 'A') })
-	brbs := lo.Map(bebs, func(beb *on.BEBChannel, _ int) *brb.BRBChannel {
-		return brb.CreateBRBChannel(n, f, beb, make(chan brb.BRBMsg))
-	})
-	tgs := lo.Map(brbs, func(brb *brb.BRBChannel, _ int) *terminationGadget { return newTerminationGadget(prefix, brb, f) })
-	on.InitializeNodes(t, nodes)
-	for _, brbChan := range brbs[:f] {
-		assert.NoError(t, brbChan.BRBroadcast(append(prefix, 0)))
-	}
-	tick := time.Tick(100 * time.Millisecond)
-	select {
-	case <-tgs[0].output:
-		t.Error("Should not have received a termination signal")
-	case <-tick:
-	}
-	for _, brbChan := range brbs[f : 2*f+1] {
-		assert.NoError(t, brbChan.BRBroadcast(append(prefix, 1)))
-	}
-	decisions := lo.Map(tgs, func(tg *terminationGadget, _ int) byte { return <-tg.output })
-	assert.True(t, lo.EveryBy(decisions, func(decision byte) bool { return decision == 1 }))
-	for _, brbChan := range brbs {
-		brbChan.Close()
-	}
-	assert.True(t, lo.EveryBy(nodes, func(node *on.Node) bool { return assert.NoError(t, node.Disconnect()) }))
+	m := newTerminationMiddleware(brbChannel)
+	assert.NoError(t, m.broadcastDecision(abaInstance, round, decision))
+	tm := <-m.output
+	assert.Equal(t, abaInstance, tm.instance)
+	assert.Equal(t, round, tm.round)
+	assert.Equal(t, decision, tm.decision)
+	assert.NotEqual(t, nil, tm.sender)
+	m.close()
 }
