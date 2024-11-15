230e09685b2472f42828a34cd099714a2d854ad8
pmcampones <p.campones@campus.fct.unl.pt>
2024-11-15 18:11:09 +0000

    More BKR tests

--- agreementCommonSubset/bkr_test.go
+++ agreementCommonSubset/bkr_test.go
@@ -5,29 +5,66 @@ import (
 	"github.com/google/uuid"
 	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
+	"math/rand/v2"
 	aba "pace/asynchronousBinaryAgreement"
 	brb "pace/byzantineReliableBroadcast"
 	ct "pace/coinTosser"
 	on "pace/overlayNetwork"
 	"slices"
 	"testing"
+	"time"
 )
 
-func TestShouldOutputOwnProposal2(t *testing.T) {
-	testShouldOutputProposalsAlt(t, 1, 0)
+func synchronousProposal(t *testing.T) func(instance *bkr, input []byte, participant uuid.UUID) {
+	return func(instance *bkr, input []byte, participant uuid.UUID) {
+		go func() {
+			assert.NoError(t, instance.receiveInput(input, participant))
+		}()
+	}
 }
 
-func TestShouldOutputProposalsNoFaults2(t *testing.T) {
-	testShouldOutputProposalsAlt(t, 10, 0)
+func TestShouldOutputOwnProposal(t *testing.T) {
+	testShouldOutputProposals(t, 1, 0, synchronousProposal(t))
 }
 
-func TestShouldOutputProposalsMaxFaults2(t *testing.T) {
+func TestShouldOutputProposalsNoFaults(t *testing.T) {
+	testShouldOutputProposals(t, 10, 0, synchronousProposal(t))
+}
+
+func TestShouldOutputProposalsMaxFaults(t *testing.T) {
 	f := uint(3)
 	n := 3*f + 1
-	testShouldOutputProposalsAlt(t, n, f)
+	testShouldOutputProposals(t, n, f, synchronousProposal(t))
 }
 
-func testShouldOutputProposalsAlt(t *testing.T, n, f uint) {
+func TestShouldOutputOwnProposalWithDelay(t *testing.T) {
+	maxDelay := uint(300)
+	testShouldOutputProposals(t, 1, 0, delayedProposal(t, maxDelay))
+}
+
+func TestShouldOutputProposalsNoFaultsWithDelay(t *testing.T) {
+	maxDelay := uint(300)
+	testShouldOutputProposals(t, 10, 0, delayedProposal(t, maxDelay))
+}
+
+func TestShouldOutputProposalsMaxFaultsWithDelay(t *testing.T) {
+	maxDelay := uint(300)
+	f := uint(3)
+	n := 3*f + 1
+	testShouldOutputProposals(t, n, f, delayedProposal(t, maxDelay))
+}
+
+func delayedProposal(t *testing.T, maxDelay uint) func(instance *bkr, input []byte, participant uuid.UUID) {
+	return func(instance *bkr, input []byte, participant uuid.UUID) {
+		go func() {
+			delay := time.Duration(rand.IntN(int(maxDelay)))
+			time.Sleep(delay * time.Millisecond)
+			assert.NoError(t, instance.receiveInput(input, participant))
+		}()
+	}
+}
+
+func testShouldOutputProposals(t *testing.T, n, f uint, receiveInput func(instance *bkr, input []byte, participant uuid.UUID)) {
 	nodes := lo.Map(lo.Range(int(n)), func(i int, _ int) *on.Node {
 		address := fmt.Sprintf("localhost:%d", 6000+i)
 		return on.GetNode(t, address, "localhost:6000")
@@ -35,16 +72,16 @@ func testShouldOutputProposalsAlt(t *testing.T, n, f uint) {
 	abachans := getAbachans(t, n, f, nodes)
 	id := uuid.New()
 	proposers := lo.Map(nodes, func(node *on.Node, _ int) uuid.UUID { return uuid.New() })
-	bkr2Instances := lo.Map(abachans, func(abachan *aba.AbaChannel, _ int) *bkr {
+	bkrInstances := lo.Map(abachans, func(abachan *aba.AbaChannel, _ int) *bkr {
 		return newBKR(id, f, proposers, abachan)
 	})
-	for _, bkr := range bkr2Instances {
+	for _, bkr := range bkrInstances {
 		for i, participant := range proposers {
 			input := []byte(fmt.Sprintf("input%d", i))
-			assert.NoError(t, bkr.receiveInput(input, participant))
+			receiveInput(bkr, input, participant)
 		}
 	}
-	outputs := lo.Map(bkr2Instances, func(bkr *bkr, _ int) [][]byte { return <-bkr.output })
+	outputs := lo.Map(bkrInstances, func(bkr *bkr, _ int) [][]byte { return <-bkr.output })
 	assert.True(t, uint(len(outputs[0])) >= f)
 	firstOutput := outputs[0]
 	assert.True(t, lo.EveryBy(outputs, func(output [][]byte) bool { return equalsOutputs(output, firstOutput) }))
